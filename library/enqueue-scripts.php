54ed644b335cc77b146c6b4f819860e2dbebeff7
steelbridge-io <chris@steelbridge.io>
2024-08-14 11:21:22 -0700

    Rename custom.css to parent-theme-tfs-custom.css
    Updated all theme enqueuing scripts to use the renamed parent theme CSS file. This ensures consistency across all themes and supports better organization and management of stylesheets.

--- library/enqueue-scripts.php
+++ library/enqueue-scripts.php
@@ -21,7 +21,7 @@ if ( ! function_exists( 'guideservice_scripts' ) ) :
 		wp_enqueue_style('main-stylesheet', get_template_directory_uri() . '/dist/assets/css/' . foundationpress_asset_path('app.css'), array(), '2.10.4', 'all');
 		
 		// Parent Theme CSS
-		wp_enqueue_style('custom-style', get_template_directory_uri() . '/assets/css/custom.css', array(), '1.0.0', 'all');
+		wp_enqueue_style('custom-style', get_template_directory_uri() . '/assets/css/parent-theme-tfs-custom.css', array(), '1.0.0', 'all');
 		
 		// Child Theme CSS
 		wp_enqueue_style( 'child-style',get_stylesheet_directory_uri() . '/style.css', array(), '1.0.0', 'all' );
